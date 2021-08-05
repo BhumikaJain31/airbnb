2b829a58f4b0a1cd4b326d7370354bb1206dc2e4
Bhumika <jainbhumi.hsr@gmail.com>
2021-08-05 19:33:37 +0530

    Modified  day-1

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Image from "next/image"
+import Profile from "../public/image.jpg";
+
 function Banner() {
     return (
-        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-            <Image src="https:links.papareact.com/0fm" 
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[650px]">
+            <Image src={Profile}
             layout="fill" objectFit="cover" />
             <div className="absolute text-center top-1/2 w-full">
                 <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
