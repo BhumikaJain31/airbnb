b515bed12d9d2f81c9274eb2f25638fbb3583661
Bhumika <jainbhumi.hsr@gmail.com>
2021-08-07 00:28:35 +0530

    Changed env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
     images: {
         domains: ["links.papareact.com"]
     },
-    
+    env: {
+        mapbox_key: "pk.eyJ1IjoiYmh1bWlrYWphaW4zMSIsImEiOiJja3MwaTZlMm0wYWh3MnZwZW1vZWtxNjQ4In0.6kKW84OftO1G0-qQXwMcJw"
+    }
 }
 
